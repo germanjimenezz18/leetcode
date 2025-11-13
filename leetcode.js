2e3287793c3f09580ea006b74ab4e6fb642105d5
G. Jimenez <germanjimenezz18@gmail.com>
2025-11-13 02:10:15 +0100

    init 2am kekw

---
+++ leetcode.js
@@ -0,0 +1,91 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+const expect = (val: any) => {
+  return {
+    toBe: (compareVal: any) => {
+      if (val === compareVal) {
+        return true;
+      } else {
+        throw Error("Not Equal");
+      }
+    },
+    notToBe: (compareVal: any) => {
+      if (val !== compareVal) {
+        return true;
+      } else {
+        throw Error("Equal");
+      }
+    },
+  };
+};
+// const result = expect(5).toBe(5);
+// console.log(result);
+
+/* ///////////////////////////////////////////////////////////// */
+
+var createCounter = function (init: any) {
+  const originalValue = init;
+  let current = init || 0;
+
+  return {
+    increment: () => {
+      return current++;
+    },
+    decrement: () => {
+      return current--;
+    },
+    reset: () => {
+      current = originalValue;
+      return current;
+    },
+  };
+};
+
+var map = function (arr, fn) {
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    result.push(fn(arr[i], i));
+  }
+  return result;
+};
+
+
+// 
+// ┌───────────────────────────────┐
+// │ 2634. Filter Elements from Array │
+// └───────────────────────────────┘
+var filter = function (arr, fn) {
+  const filteredArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    const k = fn(arr[i], i);
+    if (k) filteredArray.push(arr[i]);
+  }
+  return filteredArray;
+};
+
+/* Filter with reduce */
+const filterFunctional = function (arr, fn) {
+  return arr.reduce((filtered, value, index) => {
+    return fn(value, index) ? [...filtered, value] : filtered;
+  }, []);
+};
+
+// 
+// ┌───────────────────────────────┐
+// │ 2626. Array Reduce Transformation │
+// └───────────────────────────────┘
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function (nums, fn, init) {
+  let res = init;
+  for (i = 0; i < nums.length; i++) {
+    res = fn(res, nums[i]);
+  }
+  return res;
+};
