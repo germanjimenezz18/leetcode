c722ab70a361ecae934b67879c5433328ba1a302
G. Jimenez <germanjimenezz18@gmail.com>
2025-11-14 17:29:41 +0100

    feat: 2621, 2715

--- leetcode.js
+++ leetcode.js
@@ -177,3 +177,63 @@ async function addTwoPromises(promise1, promise2) {
  * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
  *   .then(console.log); // 4
  */
+
+// ┌───────────────────────────────────────────────┐
+// │ 2621. Sleep 								   │
+// └───────────────────────────────────────────────┘
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+	return new Promise((resolve) => setTimeout(resolve, millis));
+}
+/**
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
+
+//
+// ┌───────────────────────────────────────────────┐
+// │ 2715. Timeout Cancellation 				   │
+// └───────────────────────────────────────────────┘
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+
+function cancellable(fn, args, t) {
+	const timeoutId = setTimeout(() => {
+		fn(...args);
+	}, t);
+
+	return function () {
+		return clearTimeout(timeoutId);
+	};
+}
+
+/**
+ *  const result = [];
+ *
+ *  const fn = (x) => x * 5;
+ *  const args = [2], t = 20, cancelTimeMs = 50;
+ *
+ *  const start = performance.now();
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr)});
+ *  }
+ *
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  const maxT = Math.max(t, cancelTimeMs);
+ *
+ *  setTimeout(cancel, cancelTimeMs);
+ *
+ *  setTimeout(() => {
+ *      console.log(result); // [{"time":20,"returned":10}]
+ *  }, maxT + 15)
+ */
