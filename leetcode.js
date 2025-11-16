f9a3b24f92c84fbc6e021713149fca5990a444e9
G. Jimenez <germanjimenezz18@gmail.com>
2025-11-16 03:53:14 +0100

    feat: 2627

--- leetcode.js
+++ leetcode.js
@@ -361,3 +361,27 @@ TimeLimitedCache.prototype.count = function () {
  * timeLimitedCache.get(1) // 42
  * timeLimitedCache.count() // 1
  */
+
+// ┌───────────────────────────────────────────────┐
+// │ 2627. Debounce 				               │
+// └───────────────────────────────────────────────┘
+
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+var debounce = function (fn, t) {
+	let timer = null;
+	return function (...args) {
+		if (timer) clearTimeout(timer);
+		timer = setTimeout(() => fn(...args), t);
+	};
+};
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
